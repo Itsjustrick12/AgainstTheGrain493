5890f6df193dd31a6fb7fd1ee06ef07766e285b5
CristianUribe293 <curibe1@umbc.edu>
2026-03-28 19:33:11 -0400

    Almost Finished Implementation

--- Assets/Scripts/UI/UnitInfoPanel.cs
+++ Assets/Scripts/UI/UnitInfoPanel.cs
@@ -1,21 +1,40 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class UnitInfoPanel : MonoBehaviour
 {
-    [SerializeField] private UINumber currentHealthNum;
-    [SerializeField] private UINumber maxHealthNum;
-    [SerializeField] private UINumber strength;
-    [SerializeField] private UINumber movementRange;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI strengthText;
+    [SerializeField] private TextMeshProUGUI movementText;
+    [SerializeField] private CanvasGroup currentCanvas;
+
 
     public void ShowPanel(Unit currUnit)
     {
+        currentCanvas.alpha = 1;
+        currentCanvas.interactable = true;
+        currentCanvas.blocksRaycasts = true; //Prevents things from behind it being clicked
         UnitInfo info = UnitDatabase.Instance.GetUnitInfo(currUnit.ID);
-        currentHealthNum.UpdateDigit(currUnit.GetHealth());
-        maxHealthNum.UpdateDigit(info.baseHealth);
-        strength.UpdateDigit(info.strength);
-        movementRange.UpdateDigit(info.moveRange);
+        string unitName = info.name;
+        int currentHealth = currUnit.GetHealth();
+        int maxHealth = info.baseHealth;
+        int strength = info.strength;
+        int moveRange = info.moveRange;
 
+        nameText.text = unitName;
+        healthText.text = "Health: " + currentHealth + " / " + maxHealth;
+        strengthText.text = "Strength: " + strength;
+        movementText.text = "Movement Range: " + moveRange;
+
+    }
+
+    public void HidePanel()
+    {
+        currentCanvas.alpha = 0;
+        currentCanvas.interactable = false;
+        currentCanvas.blocksRaycasts = false; //Prevents things from behind it being clicked
     }
 
 }
