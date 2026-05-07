2c6018e53222b21f552dfdbcd9f1ce68ee0231f8
Mindstormer98 <hwaletti@gmail.com>
2026-05-07 19:11:31 -0400

    update
    synched properly

--- Assets/Scripts/UI/TurnChangeUI.cs
+++ Assets/Scripts/UI/TurnChangeUI.cs
@@ -5,19 +5,22 @@ public class TurnChangeUI : MonoBehaviour
 {
     public static UnityEvent TurnAnimationEnd = new UnityEvent();
     public Animator anim;
+    public EnemyTurnOverlay enemyOverlay;
     public void EndAnimation()
     {
         TurnAnimationEnd?.Invoke();
         anim.SetTrigger("idle");
     }
 
-   public void PlayPlayerTurn()
-   {
-        anim.SetTrigger("playerTurn");
-   }
+    public void PlayPlayerTurn()
+    {
+            enemyOverlay.PlayerTurn();
+            anim.SetTrigger("playerTurn");
+    }
 
     public void PlayEnemyTurn()
     {
+        enemyOverlay.EnemyTurn();
         anim.SetTrigger("enemyTurn");
     }
 }
